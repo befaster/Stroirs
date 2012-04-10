f0f373cec2d8871088db473a5a10c876155271b9
Dmitriy Dovgiy <dmitriy@.(none)>
2012-04-11 01:55:02 +0300

    new theme

--- sites/all/themes/stroirs/js/hover-image.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$(".lightbox-image").append("<span></span>");
-	$(".lightbox-image").hover(function(){
-		$(this).find("span").stop().delay(300).animate({left:'106px'}, 500, 'easeOutBack')},
-		function(){
-		$(this).find("span").stop().animate({left:'132px'}, 500, 'easeOutBack')})
-		})
-		
-		$(function(){$(".lightbox-image1").append("<span></span>");$(".lightbox-image1").hover(function(){$(this).find("img").stop().animate({opacity:0.5},"normal")},function(){$(this).find("img").stop().animate({opacity:1},"normal")})})
