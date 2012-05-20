e5001e3457e25cda0e9a571f34f764ef006683d7
Dmitriy Dovgiy <dmitriy@.(none)>
2012-05-20 13:06:37 +0300

    sub menu

--- sites/all/themes/stroirs/js/init.js
+++ sites/all/themes/stroirs/js/init.js
@@ -5,7 +5,7 @@ jQuery(function($){
 		children: 'div',
 		transition: 'fade',
 		animationSpeed: 800,
-		autoplay: false,
+		autoplay: true,
 		autoplaySpeed: 3000,
 		pauseOnHover: false,
 		bullets: false,
