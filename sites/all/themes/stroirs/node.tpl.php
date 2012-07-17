e9fc52c017edcf3b2ad60f474e68a2c012311818
Dmitriy Dovgiy <dmitriy@.(none)>
2012-07-17 22:35:35 +0300

    ddd

--- sites/all/themes/stroirs/node.tpl.php
+++ sites/all/themes/stroirs/node.tpl.php
@@ -1,32 +1,36 @@
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
-  <?php print $user_picture; ?>
+    <?php print $user_picture; ?>
 
-  <?php print render($title_prefix); ?>
-  <?php if (!$page): ?>
-    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
+    <?php print render($title_prefix); ?>
+    <?php if (!$page): ?>
+        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
 
-  <?php if ($display_submitted): ?>
-    <div class="submitted"><?php print $submitted ?></div>
-  <?php endif; ?>
-
-  <div class="content clearfix"<?php print $content_attributes; ?>>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      print render($content);
-    ?>
-  </div>
-
-  <div class="clearfix">
-    <?php if (!empty($content['links'])): ?>
-      <div class="links"><?php print render($content['links']); ?></div>
+    <?php if ($display_submitted): ?>
+        <div class="submitted"><?php print $submitted ?></div>
     <?php endif; ?>
 
-    <?php print render($content['comments']); ?>
-  </div>
+    <div class="content clearfix"<?php print $content_attributes; ?>>
+        <?php
+        // We hide the comments and links now so that we can render them later.
+        hide($content['comments']);
+        hide($content['links']);
+        print render($content);
+        ?>
+
+        <?php if (!empty($node->field_article_price['und'][0]['value'])) : ?>
+        <div class="price-buttons"><a class="button" href="/prices#<?php echo $node->field_article_price['und'][0]['value']; ?>">Стоимость</a></div>
+        <?php endif; ?>
+    </div>
+
+    <div class="clearfix">
+        <?php if (!empty($content['links'])): ?>
+            <div class="links"><?php print render($content['links']); ?></div>
+        <?php endif; ?>
+
+        <?php print render($content['comments']); ?>
+    </div>
 
 </div>
