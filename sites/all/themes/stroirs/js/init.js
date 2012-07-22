f32675dd698a4df7b9c3d670c4b34c64fca22123
Dmitriy Dovgiy <dmitriy@.(none)>
2012-07-22 15:08:12 +0300

    new

--- sites/all/themes/stroirs/js/init.js
+++ sites/all/themes/stroirs/js/init.js
@@ -1,11 +1,20 @@
-jQuery(function($){	
-    $('.slider').mobilyslider({
-        content: '.sliderContent',
-        children: 'div',
-        transition: 'fade',
-        animationSpeed: 800,
-        bullets: false,
-        arrowsHide: true        
-    });
+jQuery(function($){
+	
+	$('.slider').mobilyslider({
+		content: '.sliderContent',
+		children: 'div',
+		transition: 'fade',
+		animationSpeed: 1000,
+		autoplay: true,
+		autoplaySpeed: 1500,
+		pauseOnHover: false,
+		bullets: false,
+		arrows: true,
+		arrowsHide: true,
+		prev: 'prev',
+		next: 'next',
+		animationStart: function(){},
+		animationComplete: function(){}
+	});
 	
 });
