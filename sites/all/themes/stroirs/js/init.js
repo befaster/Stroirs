0f67de285d4b760e06c573a605c5e7eee239544b
Dmitriy Dovgiy <dmitriy@.(none)>
2012-07-22 13:49:57 +0300

    new

--- sites/all/themes/stroirs/js/init.js
+++ sites/all/themes/stroirs/js/init.js
@@ -2,18 +2,16 @@ jQuery(function($){
 	$('.slider').mobilyslider({
 		content: '.sliderContent',
 		children: 'div',
-		transition: 'vertical',
-		animationSpeed: 800,
+		transition: 'fade',
+		animationSpeed: 1000,
 		autoplay: true,
-		autoplaySpeed: 2000,
+		autoplaySpeed: 1000,
 		pauseOnHover: false,
 		bullets: false,
 		arrows: true,
 		arrowsHide: true,
 		prev: 'prev',
 		next: 'next',
-		animationStart: function(){},
-		animationComplete: function(){}
 	});
 	
 });
