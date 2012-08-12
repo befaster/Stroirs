64890cd316e6eb122a2c4455b3261414d867f312
Dmitriy Dovgiy <dmitriy@.(none)>
2012-08-12 11:34:57 +0300

    added

--- sites/all/themes/stroirs/js/init.js
+++ sites/all/themes/stroirs/js/init.js
@@ -2,7 +2,7 @@ jQuery(function($){
     $('document').ready(function() {
         $('.sliderContent').mobilyslider({
            /* content: '.views-field-field-gallery-image',*/
-            children: 'img',
+            children: 'div',
             transition: 'horizontal',
             autoplay: true,
             pauseOnHover: true,
