2e54c7275ffe410fff82df2fd14563d368614288
Dmitriy Dovgiy <dmitriy@.(none)>
2012-08-12 10:50:13 +0300

    added

--- sites/all/themes/stroirs/template.php
+++ sites/all/themes/stroirs/template.php
@@ -10,7 +10,7 @@ function stroirs_preprocess_html(&$vars) {
     drupal_add_css(drupal_get_path('theme', 'stroirs') . '/css/slider.css', array('group' => CSS_THEME, 'preprocess' => FALSE));
     
     drupal_add_css('http://fonts.googleapis.com/css?family=Oswald', array('group' => CSS_THEME, 'preprocess' => FALSE));
-    drupal_add_js(drupal_get_path('theme', 'stroirs') . '/js/mobilyslider.js');
-    drupal_add_js(drupal_get_path('theme', 'stroirs') . '/js/init.js');
+    drupal_add_js(drupal_get_path('theme', 'stroirs') . '/js/wowslider.js');
+    drupal_add_js(drupal_get_path('theme', 'stroirs') . '/js/script.js');
     //drupal_add_js('jQuery(document).ready(function() {});', 'inline');
 }
