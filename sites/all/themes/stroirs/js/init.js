67afe759dc320148d44f8902b28416d9559dd108
Dmitriy Dovgiy <dmitriy@.(none)>
2012-08-12 11:57:23 +0300

    added

--- sites/all/themes/stroirs/js/init.js
+++ sites/all/themes/stroirs/js/init.js
@@ -3,7 +3,7 @@ jQuery(function($){
         $('.slider').mobilyslider({
             content: '.sliderContent',
             children: 'div',
-            transition: 'fade',
+            transition: 'horizontal',
             autoplay: true,
             pauseOnHover: false,
             bullets: false,
