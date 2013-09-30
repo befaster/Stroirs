6cbfa4d745f6a647e074f6e1e65aa05bc2086519
Dmitriy Dovgiy <swcphp@gmail.com>
2013-09-30 11:02:51 +0300

    testing

--- sites/all/themes/stroirs/js/init.js
+++ sites/all/themes/stroirs/js/init.js
@@ -1,22 +1,21 @@
-jQuery(function($){
-    
-  
-        $('.slider').mobilyslider({
-            content: '.sliderContent',
-            children: 'div',
-            transition: 'fade',            	
-            animationSpeed: 4,
-            autoplay: true,
-            pauseOnHover: true,
-            bullets: false,
-            arrows: true,
-            arrowsHide: true,
-            prev: 'prev',
-            next: 'next',
-            animationStart: function(){}, // call the function on start transition
-            animationComplete: function(){} // call the function when transition completed
-        });
-                       
-        
-  
+jQuery('document').ready(function() {
+    jQuery('.slider').mobilyslider({
+        content: '.sliderContent',
+        children: 'div',
+        transition: 'fade',
+        animationSpeed: 2,
+        autoplay: true,
+        pauseOnHover: true,
+        bullets: false,
+        arrows: true,
+        arrowsHide: true,
+        prev: 'prev',
+        next: 'next',
+        animationStart: function() {
+        }, // call the function on start transition
+        animationComplete: function() {
+        } // call the function when transition completed
+    });
+
 });
+
