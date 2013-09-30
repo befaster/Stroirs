437b6873356b19cd11853c78d1ff2fdbe219a832
Dmitriy Dovgiy <swcphp@gmail.com>
2013-09-30 11:09:46 +0300

    testing

--- sites/all/themes/stroirs/js/init.js
+++ sites/all/themes/stroirs/js/init.js
@@ -3,7 +3,7 @@ jQuery('document').ready(function() {
         content: '.sliderContent',
         children: 'div',
         transition: 'fade',
-        /*animationSpeed: 4000,*/
+        animationSpeed: 2000,
         autoplay: true,
         pauseOnHover: true,
         bullets: false,
