4caa34f3602dc77479dbb507f112fe62f92ced07
Dmitriy Dovgiy <swcphp@gmail.com>
2013-10-02 01:42:22 +0300

    testing

--- sites/all/modules/stroirs_panel/templates/stroirs_panel_main_block.tpl.php
+++ sites/all/modules/stroirs_panel/templates/stroirs_panel_main_block.tpl.php
@@ -1,2 +1,3 @@
 <a href="#" id="trigger1" class="trigger left">&nbsp;Доставка</a>
 <div id="panel1" class="panel left"><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /></div>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
