520da7263f9436b4d1d47707b4fac012c61962fb
Dmitriy Dovgiy <swcphp@gmail.com>
2013-10-01 19:53:06 +0300

    testing

--- sites/all/modules/stroirs_panel/templates/stroirs_panel_mail_block.tpl.php
+++ sites/all/modules/stroirs_panel/templates/stroirs_panel_mail_block.tpl.php
@@ -1,3 +1,4 @@
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
 <a href="#" id="trigger1" class="trigger right">panel</a>
 <div id="panel1" class="panel left">
     <p>Here will be calculate.</p>
