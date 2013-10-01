293e67e56268a966d1005ab49259ab356ba7af14
Dmitriy Dovgiy <swcphp@gmail.com>
2013-10-02 01:31:58 +0300

    testing

--- sites/all/modules/stroirs_panel/templates/stroirs_panel_main_block.tpl.php
+++ sites/all/modules/stroirs_panel/templates/stroirs_panel_main_block.tpl.php
@@ -1,2 +1,2 @@
 <a href="#" id="trigger1" class="trigger left">&nbsp;Доставка</a>
-<div id="panel1" class="panel left"></div>
+<div id="panel1" class="panel left"><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /></div>
