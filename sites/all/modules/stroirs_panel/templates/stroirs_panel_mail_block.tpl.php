87948ebf79945b107478b2e68cfcc23d5553c612
Dmitriy Dovgiy <swcphp@gmail.com>
2013-10-02 01:08:38 +0300

    testing

--- sites/all/modules/stroirs_panel/templates/stroirs_panel_mail_block.tpl.php
+++ sites/all/modules/stroirs_panel/templates/stroirs_panel_mail_block.tpl.php
@@ -1,4 +1,2 @@
-<a href="#" id="trigger1" class="trigger right">panel</a>
-<div id="panel1" class="panel left">
-    <p>Here will be calculate.</p>
-</div>
+<a href="#" id="trigger1" class="trigger left">Калькулятор</a>
+<div id="panel1" class="panel left"></div>
