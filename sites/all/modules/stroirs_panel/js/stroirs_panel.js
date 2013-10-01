85687c23178935db0c26833c915e27ebbd5f5a66
Dmitriy Dovgiy <swcphp@gmail.com>
2013-10-02 01:28:01 +0300

    testing

--- sites/all/modules/stroirs_panel/js/stroirs_panel.js
+++ sites/all/modules/stroirs_panel/js/stroirs_panel.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function() {
     jQuery('#panel1').slidePanel({
-        triggerName: '#trigger1',
+        triggerName: '#trigger1'
         /*triggerTopPos: '220px',
         panelTopPos: '200px',*/
-        ajax: true,
-        ajaxSource: '/stroirs_panel/panel'
+        /*ajax: true,
+        ajaxSource: '/stroirs_panel/panel'*/
 
     });
 });
