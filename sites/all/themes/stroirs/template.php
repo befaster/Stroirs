3f5dd629d98cde4983a7f7c86cf8d46a5292a259
Dmitriy Dovgiy <swcphp@gmail.com>
2013-10-02 02:13:06 +0300

    testing

--- sites/all/themes/stroirs/template.php
+++ sites/all/themes/stroirs/template.php
@@ -4,8 +4,8 @@
  * Override or insert variables into the html template.
  */
 function stroirs_preprocess_html(&$vars) {
-    //drupal_add_js(drupal_get_path('theme', 'stroirs') . '/js/mobilyslider.js');
-    //drupal_add_js(drupal_get_path('theme', 'stroirs') . '/js/init.js');
+    drupal_add_js(drupal_get_path('theme', 'stroirs') . '/js/mobilyslider.js');
+    drupal_add_js(drupal_get_path('theme', 'stroirs') . '/js/init.js');
 
     drupal_add_css(drupal_get_path('theme', 'stroirs') . '/css/reset.css', array('group' => CSS_THEME, 'preprocess' => FALSE));
     drupal_add_css(drupal_get_path('theme', 'stroirs') . '/css/style.css', array('group' => CSS_THEME, 'preprocess' => FALSE));
