568e8ab9362f56e30f2ac5c396d354dcb406db0d
Dmitriy Dovgiy <swcphp@gmail.com>
2013-10-02 21:37:45 +0300

    testing

--- sites/all/modules/stroirs_panel/templates/stroirs_panel_main_block.tpl.php
+++ sites/all/modules/stroirs_panel/templates/stroirs_panel_main_block.tpl.php
@@ -1,5 +1,5 @@
 <a href="#" id="strigger1" class="strigger sleft">&nbsp;Доставка</a>
 <div id="spanel1" class="spanel sleft">
-   <?php echo $form; ?>
+   <?php echo drupal_render($form); ?>
 </div>
 
