a90c798dc9592b710d055c70baeb698afbc7e75d
Dmitriy Dovgiy <swcphp@gmail.com>
2013-10-04 11:56:57 +0300

    testing

--- sites/all/modules/stroirs_panel/js/stroirs_panel.js
+++ sites/all/modules/stroirs_panel/js/stroirs_panel.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function() {
     jQuery('#spanel1').slidePanel({
         triggerName: '#strigger1',
-        triggerTopPos: '240px',
-        panelTopPos: '230px',
+        triggerTopPos: '175px',
+        panelTopPos: '165px',
         position: 'fixed',
         panelOpacity: '0.9',
         clickOutsideToClose: true
