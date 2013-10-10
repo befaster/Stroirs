be82443437f84191ccec8679820689ce1faf59db
Dmitriy Dovgiy <swcphp@gmail.com>
2013-10-10 16:15:10 +0300

    testing

--- sites/all/modules/stroirs_panel/templates/stroirs_panel_main_block.tpl.php
+++ sites/all/modules/stroirs_panel/templates/stroirs_panel_main_block.tpl.php
@@ -1,4 +1,4 @@
-<a href="#" id="strigger1" class="strigger sleft" onclicl="jQuery('#stroirs-panel-form').reset();">&nbsp;Цена доставки</a>
+<a href="#" id="strigger1" class="strigger sleft">&nbsp;Цена доставки</a>
 <div id="spanel1" class="spanel sleft">
    <?php echo drupal_render($form); ?>
 </div>
