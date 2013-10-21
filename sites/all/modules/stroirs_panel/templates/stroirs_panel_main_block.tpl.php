88b0d129fdd8903eb245ef055812f27c8320321a
Dmitriy Dovgiy <swcphp@gmail.com>
2013-10-21 08:20:21 +0300

    testing

--- sites/all/modules/stroirs_panel/templates/stroirs_panel_main_block.tpl.php
+++ sites/all/modules/stroirs_panel/templates/stroirs_panel_main_block.tpl.php
@@ -1,4 +1,4 @@
-<a href="#" id="strigger1" class="strigger sleft">Цена с доставкой</a>
+<a href="#" id="strigger1" class="strigger sleft">Цена&nbsp;&nbsp;&nbsp;с&nbsp;&nbsp;&nbsp;доставкой</a>
 <div id="spanel1" class="spanel sleft">
    <?php echo drupal_render($form); ?>
 </div>
